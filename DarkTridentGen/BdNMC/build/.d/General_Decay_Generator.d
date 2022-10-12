b807e06fc50f5a937427d738a95ad33b82888163
Luis Mora Lepin <lmoralep@uboonegpvm01.fnal.gov>
2022-10-12 14:16:04 -0500

    Removing non-compatible bdnmc

--- DarkTridentGen/BdNMC/build/.d/General_Decay_Generator.d
+++
@@ -1,8 +0,0 @@
-General_Decay_Generator.o: ../src/General_Decay_Generator.cpp \
- ../src/DMgenerator.h ../src/Particle.h ../src/Random.h
-
-../src/DMgenerator.h:
-
-../src/Particle.h:
-
-../src/Random.h:
