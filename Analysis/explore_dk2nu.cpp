30f5469cffae8946ba2f17f3f9d2211be443896a
lmlepin9 <lml24d@fsu.edu>
2024-10-06 16:45:01 -0500

    Renaming folder

---
+++ Analysis/explore_dk2nu.cpp
@@ -0,0 +1,10 @@
+#include "TFile.h"
+#include "TTree.h"
+#include <iostream>
+
+
+void explore_dk2nu(){
+    TFile *f = new TFile("g4numiv6_minervame_me000z200i_2_0000.root","READ");
+    
+
+}
---
+++ Analysis/limit_utils.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+
+def SetScaling(n_signal,n_back):
+    scale_factor = 2. 
+    if(n_signal > n_back):
+     
+    else:
+        
